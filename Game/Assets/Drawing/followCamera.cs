1d09d7aae7e5358655bf72117b463df4d0f8c076
atonyma7 <atonyma7@gmail.com>
2020-02-01 21:26:25 -0700

    follows camera

---
+++ Game/Assets/Drawing/followCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 temp = Input.mousePosition;
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
